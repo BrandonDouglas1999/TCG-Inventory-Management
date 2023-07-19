50e8a702362d4420c3f5355eece10ef094ab91f5
BrandonDoulgas <douglasb25@mymacewan.ca>
2023-07-18 18:53:30 -0600

    Add shopping cart and card classes

---
+++ InventoryApp/ShoppingCart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp
+{
+    internal class ShoppingCart
+    {
+        public List<Card> cart;
+
+        public ShoppingCart()
+        {
+            cart = new List<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            cart.Add(card);
+        }
+
+        public void DeleteCard(Card card)
+        {
+            cart.Remove(card);
+        }
+
+        public void Clear()
+        {
+            cart.Clear();
+        }
+
+        public List<Card> GetCart()
+        {
+            return cart;
+        }
+
+        public Card ViewCart(int index)
+        {
+            return cart[index];
+        }
+    }
+}
