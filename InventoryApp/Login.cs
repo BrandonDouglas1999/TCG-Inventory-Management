3dd637ac96bc9363169cfaccba4830dfeb152418
Ken <kennethlieu@gmail.com>
2023-07-26 20:16:41 -0600

    Can get the access token now

--- InventoryApp/Login.cs
+++ InventoryApp/Login.cs
@@ -12,11 +12,22 @@ using System.Security.Cryptography;
 using System.Net;
 using System.Diagnostics;
 using System.Collections.Specialized;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Auth.AspNetCore3;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Encodings.Web;
 
 namespace InventoryApp
 {
     public partial class Login : Form
     {
+
+        const string clientID = "657611740650-i9i8o0846cs58juodncpo44hgpj8ju6s.apps.googleusercontent.com";
+        const string secret = "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";
+        const string redirect_uri = $"http://127.0.0.1:15000/";
+
+        HttpListener response_listen = new HttpListener();
+
 
         public bool authenticated = false;
         public Login()
@@ -52,22 +63,28 @@ namespace InventoryApp
         private void google_logo_Click(object sender, EventArgs e)
         {
 
-            HttpListener get_code = new HttpListener();
+            string authCode = getAuthCode();
+            getToken(authCode);
+
+            return;
+        }
+
+        private string getAuthCode()
+        {
+
             string auth = $"https://accounts.google.com/o/oauth2/v2/auth?" +
                 $"scope=profile%20email" +
                 $"&response_type=code&" +
-                $"redirect_uri=http://{IPAddress.Loopback}:15000/&" +
-                $"client_id=657611740650-i9i8o0846cs58juodncpo44hgpj8ju6s.apps.googleusercontent.com";
-
-            Debug.Print(auth);
+                $"redirect_uri={redirect_uri}&" +
+                $"client_id={clientID}";
 
             // Create a URI to redirect the authorization code to on port 15000
 
             string redirect_URI = $"http://{IPAddress.Loopback}:15000/";
 
             // Listen for the OAuth token.
-            get_code.Prefixes.Add(redirect_URI);
-            get_code.Start();
+            response_listen.Prefixes.Add(redirect_URI);
+            response_listen.Start();
 
             ProcessStartInfo auth_url = new ProcessStartInfo(auth);
             auth_url.UseShellExecute = true;
@@ -75,9 +92,7 @@ namespace InventoryApp
 
             // From the HttpListener, get the context and extract the authorized code
             // Add some kind of error check here too
-            HttpListenerContext context = get_code.GetContext();
-
-            //string code = context.Request.QueryString.Get("code");
+            HttpListenerContext context = response_listen.GetContext();
 
             // Query string is a NameValueCollection (basically a dictionary)
             // As of right now, contains the following keys:
@@ -88,10 +103,37 @@ namespace InventoryApp
 
             // If there is an error, error is the only key.
 
+            string code = context.Request.QueryString.Get("code");
+            response_listen.Stop();
+            return code;
+        }
 
-            get_code.Stop();
+        private async void getToken(string authCode)
+        {
+            // Create the Token request 
 
-            return;
+            string endpoint = "https://oauth2.googleapis.com/token";
+
+            var needed_info= new Dictionary<string, string>
+            {
+                {"code", authCode},
+                {"client_id", clientID},
+                {"client_secret", secret},
+                {"redirect_uri", redirect_uri},
+                {"grant_type", "authorization_code"},
+            };
+
+
+            var formatted_info = new FormUrlEncodedContent(needed_info);
+
+            HttpClient client = new HttpClient();
+
+            var response = await client.PostAsync(endpoint, formatted_info);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            Debug.WriteLine(result);
+
         }
     }
 }
