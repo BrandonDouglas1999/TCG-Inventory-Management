9fa43bc77fd1b027507033aa07d8f73d0c7e7415
BrandonDoulgas <douglasb25@mymacewan.ca>
2023-08-18 10:49:16 -0600

    Slight UI fix

--- InventoryApp/home_page.Designer.cs
+++ InventoryApp/home_page.Designer.cs
@@ -54,9 +54,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = Color.Black;
-            label2.Location = new Point(369, 391);
+            label2.Location = new Point(422, 521);
             label2.Name = "label2";
-            label2.Size = new Size(53, 19);
+            label2.Size = new Size(66, 24);
             label2.TabIndex = 15;
             label2.Text = "Price:";
             // 
@@ -65,9 +65,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(163, 391);
+            label1.Location = new Point(186, 521);
             label1.Name = "label1";
-            label1.Size = new Size(55, 19);
+            label1.Size = new Size(67, 24);
             label1.TabIndex = 14;
             label1.Text = "Date: ";
             // 
@@ -76,9 +76,9 @@
             rate_label.AutoSize = true;
             rate_label.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point);
             rate_label.ForeColor = Color.Black;
-            rate_label.Location = new Point(422, 391);
+            rate_label.Location = new Point(482, 521);
             rate_label.Name = "rate_label";
-            rate_label.Size = new Size(0, 19);
+            rate_label.Size = new Size(0, 24);
             rate_label.TabIndex = 13;
             // 
             // date_label
@@ -86,27 +86,27 @@
             date_label.AutoSize = true;
             date_label.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point);
             date_label.ForeColor = Color.Black;
-            date_label.Location = new Point(215, 391);
+            date_label.Location = new Point(246, 521);
             date_label.Name = "date_label";
-            date_label.Size = new Size(0, 19);
+            date_label.Size = new Size(0, 24);
             date_label.TabIndex = 12;
             // 
             // chart2
             // 
             chart2.ForeColor = SystemColors.Control;
-            chart2.Location = new Point(778, 413);
-            chart2.Margin = new Padding(4, 3, 4, 3);
+            chart2.Location = new Point(889, 551);
+            chart2.Margin = new Padding(5, 4, 5, 4);
             chart2.Name = "chart2";
-            chart2.Size = new Size(630, 325);
+            chart2.Size = new Size(720, 433);
             chart2.TabIndex = 11;
             // 
             // chart
             // 
             chart.ForeColor = SystemColors.Control;
-            chart.Location = new Point(17, 413);
-            chart.Margin = new Padding(4, 3, 4, 3);
+            chart.Location = new Point(19, 551);
+            chart.Margin = new Padding(5, 4, 5, 4);
             chart.Name = "chart";
-            chart.Size = new Size(611, 325);
+            chart.Size = new Size(698, 433);
             chart.TabIndex = 10;
             // 
             // increaseTable
@@ -138,12 +138,14 @@
             dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
             increaseTable.DefaultCellStyle = dataGridViewCellStyle3;
-            increaseTable.Location = new Point(60, 115);
+            increaseTable.Location = new Point(69, 153);
+            increaseTable.Margin = new Padding(3, 4, 3, 4);
             increaseTable.Name = "increaseTable";
             increaseTable.ReadOnly = true;
             increaseTable.RowHeadersVisible = false;
+            increaseTable.RowHeadersWidth = 51;
             increaseTable.RowTemplate.Height = 25;
-            increaseTable.Size = new Size(895, 215);
+            increaseTable.Size = new Size(1023, 287);
             increaseTable.TabIndex = 16;
             increaseTable.CellContentClick += increaseTable_CellContentClick;
             // 
@@ -152,9 +154,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Constantia", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = Color.Black;
-            label3.Location = new Point(60, 76);
+            label3.Location = new Point(69, 101);
             label3.Name = "label3";
-            label3.Size = new Size(216, 23);
+            label3.Size = new Size(273, 29);
             label3.TabIndex = 17;
             label3.Text = "Notable Price Changes:";
             // 
@@ -163,9 +165,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Constantia", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ForeColor = Color.Black;
-            label4.Location = new Point(997, 370);
+            label4.Location = new Point(1139, 493);
             label4.Name = "label4";
-            label4.Size = new Size(211, 23);
+            label4.Size = new Size(265, 29);
             label4.TabIndex = 18;
             label4.Text = "Exchange Rate History";
             // 
@@ -174,18 +176,18 @@
             label5.AutoSize = true;
             label5.Font = new Font("Constantia", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label5.ForeColor = Color.Black;
-            label5.Location = new Point(243, 358);
+            label5.Location = new Point(278, 477);
             label5.Name = "label5";
-            label5.Size = new Size(170, 23);
+            label5.Size = new Size(214, 29);
             label5.TabIndex = 19;
             label5.Text = "Card Rate History";
             // 
             // separator
             // 
             separator.BorderStyle = BorderStyle.Fixed3D;
-            separator.Location = new Point(705, 420);
+            separator.Location = new Point(806, 560);
             separator.Name = "separator";
-            separator.Size = new Size(2, 300);
+            separator.Size = new Size(2, 400);
             separator.TabIndex = 20;
             // 
             // logged_user
@@ -193,9 +195,9 @@
             logged_user.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             logged_user.AutoSize = true;
             logged_user.Font = new Font("Sylfaen", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            logged_user.Location = new Point(163, 22);
+            logged_user.Location = new Point(198, 29);
             logged_user.Name = "logged_user";
-            logged_user.Size = new Size(67, 25);
+            logged_user.Size = new Size(79, 31);
             logged_user.TabIndex = 2;
             logged_user.Text = "label1";
             // 
@@ -204,9 +206,9 @@
             user_label.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             user_label.AutoSize = true;
             user_label.Font = new Font("Sylfaen", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            user_label.Location = new Point(17, 22);
+            user_label.Location = new Point(19, 29);
             user_label.Name = "user_label";
-            user_label.Size = new Size(149, 25);
+            user_label.Size = new Size(183, 31);
             user_label.TabIndex = 1;
             user_label.Text = "Welcome back,";
             // 
@@ -215,13 +217,14 @@
             top_back_panel.BackColor = SystemColors.ControlDark;
             top_back_panel.Controls.Add(logged_user);
             top_back_panel.Location = new Point(0, 0);
+            top_back_panel.Margin = new Padding(3, 4, 3, 4);
             top_back_panel.Name = "top_back_panel";
-            top_back_panel.Size = new Size(1497, 63);
+            top_back_panel.Size = new Size(1711, 84);
             top_back_panel.TabIndex = 21;
             // 
             // home_page
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(user_label);
             Controls.Add(separator);
@@ -236,8 +239,9 @@
             Controls.Add(chart2);
             Controls.Add(chart);
             Controls.Add(top_back_panel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "home_page";
-            Size = new Size(1500, 750);
+            Size = new Size(1714, 1000);
             ((System.ComponentModel.ISupportInitialize)increaseTable).EndInit();
             top_back_panel.ResumeLayout(false);
             top_back_panel.PerformLayout();
